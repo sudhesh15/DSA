222a2ba98b6c18aa727d47dc28ce283fc8245615
Sudhesh15 <sudheshholla15@gmail.com>
2023-10-07 22:53:27 +0530

    Day 07/100 Check if Array is Sorted & Rotated

---
+++ 0. Arrays/15.cpp
@@ -0,0 +1,30 @@
+//Check if Array is Sorted & Rotated
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[5] = {3, 4, 5, 1, 2};
+    int size = 5;
+
+    int count = 0;
+    for(int i = 1; i < size; i++) {
+      if(arr[i - 1] < arr[i]) {
+        count++;
+      }
+    }
+
+    if(arr[size - 1] > arr[0]) {
+      count++;
+    }
+
+    bool isSortedAndRotated = (count == 1);
+
+    if(isSortedAndRotated) {
+      cout << "Array is sorted and rotated." << endl;
+    } else {
+      cout << "Array is not sorted and rotated." << endl;
+    }
+
+    return 0;
+}
