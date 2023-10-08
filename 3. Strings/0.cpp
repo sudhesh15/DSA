c4f462e9b86724a968646bf196115e4bb231590b
Sudhesh15 <sudheshholla15@gmail.com>
2023-10-08 21:46:05 +0530

    Day 08/100 String - Some Rough Work

---
+++ 3. Strings/0.cpp
@@ -0,0 +1,14 @@
+//String - Some Rough Work
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  char name[20];
+  cout<<"Enter your name "<<endl;
+  cin>>name;
+
+  cout<<"Hi "<<name<<endl;
+  cout<<"Hi "<<name.length()<<endl;
+  return 0;
+}
