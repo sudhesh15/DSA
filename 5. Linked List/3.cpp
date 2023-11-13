4c8b2708ad8569af260db237a91d6cf0291cff67
Sudhesh15 <sudheshholla15@gmail.com>
2023-11-13 17:42:41 +0530

    Insert Node at given Position & Printing a Linked List

---
+++ 5. Linked List/3.cpp
@@ -0,0 +1,66 @@
+//Insert Node at given Position & Printing a Linked List
+#include <iostream>
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* next;
+
+  Node(int data){
+    this -> data = data;
+    this -> next = NULL;
+  }
+};
+
+void insertAtHead(Node* &head, int data){
+  Node* temp = new Node(data);
+  temp -> next = head;
+  head = temp;
+}
+
+void insertAtTail(Node* &tail, int data){
+  Node* temp = new Node(data);
+  tail -> next = temp;
+  tail = temp;
+}
+
+void insertAtGivenPosition(Node* &head, int position, int data){
+  if(position == 1){
+    insertAtHead(head, data);
+  }
+  Node* temp = head;
+  int ctn=1;
+  while(ctn<position-1){
+    temp = temp -> next;
+    ctn++;
+  }
+  Node* newNode = new Node(data);
+  newNode -> next = temp -> next;
+  temp -> next = newNode;
+}
+
+void printLinkedList(Node* &head){
+  Node* temp = head;
+  while(temp != NULL){
+    cout<<temp -> data <<" ";
+    temp = temp -> next;
+  }
+  cout<<endl;
+}
+
+
+int main(){
+  Node* node1 = new Node(10);
+  cout<<node1 -> data <<endl;
+  cout<<node1 -> next <<endl;
+
+  Node* head = node1; //head pointed to node1
+  Node* tail = node1; //head pointed to node1
+  insertAtHead(head, 5);
+  insertAtTail(tail, 15);
+  insertAtGivenPosition(head, 3, 12);
+  printLinkedList(head);
+
+  return 0;
+}
