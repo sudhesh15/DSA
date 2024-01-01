1482781c0df22d52fc3e8a86d3796b310f7e2717
sudhesh.holla <sudheshholla15@gmail.com>
2024-01-01 23:41:19 +0530

    Max Consecutive Ones

---
+++ 0. Arrays/31.cpp
@@ -0,0 +1,18 @@
+//Max Consecutive Ones
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+       int ans = 0, count = 0;
+       for(int i=0;i<nums.size();i++){
+           if(nums[i] == 1){
+               count++;
+               if(count>ans){
+                   ans = count;
+               }
+           }else{
+               count=0;
+           }
+       } 
+       return ans;
+    }
+};
