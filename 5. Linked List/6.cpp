b9c9c34c27e8fab892caa264094eb4333455bfa6
sudhesh.holla <sudheshholla15@gmail.com>
2024-01-06 22:22:29 +0530

    Intersection of Two Linked Lists

---
+++ 5. Linked List/6.cpp
@@ -0,0 +1,48 @@
+//Intersection of Two Linked Lists
+class Solution {
+public:
+    int length(ListNode* head){
+        int l=0;
+        ListNode* temp = head;
+        while(temp != NULL){
+            temp = temp->next;
+            l++;
+        }
+        return l;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int d=0;
+        int l1 = length(headA);
+        int l2 = length(headB);
+        ListNode* ptr1;
+        ListNode* ptr2;
+
+        if(l1>l2){
+            d=l1-l2;
+            ptr1=headA;
+            ptr2=headB;
+        }else{
+            d=l2-l1;
+            ptr1=headB;
+            ptr2=headA;
+        }
+
+        while(d){
+            ptr1=ptr1->next;
+            if(ptr1 == NULL){
+                return nullptr;
+            }
+            d--;
+        }
+
+        while(ptr1 != NULL && ptr2 != NULL){
+            if(ptr1==ptr2){
+                return ptr1;
+            }
+            ptr1=ptr1->next;
+            ptr2=ptr2->next;
+        }
+
+        return nullptr;
+    }
+};
